a6aee7fd697f13233457f94a57e61d55bc67a672
SzGajdzica <sz.gajdzica@labmatic.com>
2020-03-24 10:46:43 +0100

    updated data source

--- src/main/java/pl/polsl/task/manager/rest/api/configuration/DataSourceConfig.java
+++ src/main/java/pl/polsl/task/manager/rest/api/configuration/DataSourceConfig.java
@@ -16,10 +16,17 @@ public class DataSourceConfig {
     @Bean
     public DataSource getDataSource() throws UnknownHostException {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
         if(InetAddress.getLocalHost().getHostAddress().contains("192.168")){
+            System.out.println("STARTING DATABASE AT LOCALHOST");
             dataSourceBuilder.url("jdbc:postgresql://localhost:1234/task-manager");
             dataSourceBuilder.username("postgres");
             dataSourceBuilder.password("12345");
+        } else {
+            System.out.println("STARTING DATABASE AT HEROKU");
+            dataSourceBuilder.url("jdbc:postgresql://fguyjifdlvweyi:576870d9f31bedafda3724636fe00799235921fd3919aad57647447f914371c9@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/d30pnjo54sfi93");
+            dataSourceBuilder.username("fguyjifdlvweyi");
+            dataSourceBuilder.password("576870d9f31bedafda3724636fe00799235921fd3919aad57647447f914371c9");
         }
         return dataSourceBuilder.build();
     }
